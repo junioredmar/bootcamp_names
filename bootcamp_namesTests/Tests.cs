0893edfbf828581e6c308591b7f54cb792dd5cb3
Nicholas Besagni <nicholasbesagni@MacBook-Pro-10.local>
2019-02-21 16:00:34 +0000

    Added test project

---
+++ bootcamp_namesTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace bootcamp_namesTests
+{
+    //[TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
