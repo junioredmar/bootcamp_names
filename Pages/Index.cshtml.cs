3fe851aaed5961e5c5799b7746ae07edb1493d3e
Edmar Junior <edmar@MacBook-Pro-5.local>
2019-02-21 09:39:57 +0000

    Including database

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,17 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 namespace bootcamp_names.Pages
 {
     public class IndexModel : PageModel
     {
 
+        private IMongoCollection<Users> _users;
+        private IConfiguration _config;
+
+        public IndexModel(IConfiguration config)
+        {
+            _config = config;
+        }
         public string Users { get; private set; } = "PageModel in C#";
 
-        public void OnGet()
+        public async Task OnGet()
         {
-            Users += $"Edmar";
+
+            var client = new MongoClient(_config.GetConnectionString("UsersDb"));
+            var database = client.GetDatabase("UsersDb");
+            _users = database.GetCollection<Users>("Users");
+
+            // Users userOne = new Users {
+            //     Name= "Edmar"
+            // };
+            // _users.InsertOne(userOne);
+
+            string myName = "Test";           
+            var iterator = await _users.Find(p => true).ToListAsync();
+            foreach(var user in iterator){
+                myName = user.Name;
+            }
+            Users += myName;
         }
     }
 }
